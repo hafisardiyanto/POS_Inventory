9781100c3ecfecb9a27a524fc0fa0458b69c92ab
HafisArdiyanto <hafisardiyanto19@gmail.com>
2026-02-02 22:06:38 +0700

    menambahkan jika saya search nama item nya muncul modal

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -121,6 +121,19 @@ class TransactionController extends Controller
         ]);
     }
 
+    public function items_html(): View {
+    return view('transaction.items', [
+        'items' => Item::orderBy('name')->get()
+    ]);
+}
+
+public function items_json(): JsonResponse {
+    return response()->json(
+        Item::select('id', 'code', 'name')->get()
+    );
+}
+
+
     public function get_invoice(): string
     {
         if (count(Transaction::whereDate('created_at', Carbon::today())->get()) > 0) {
@@ -133,4 +146,6 @@ class TransactionController extends Controller
         $invoice = env('INVOICE_PREFIX') . date('dmy') . str_pad($invoice, 4, "0", STR_PAD_LEFT);
         return json_encode(['invoice' => $invoice, 'invoice_no' => $invoice_no]);
     }
+
+
 }
